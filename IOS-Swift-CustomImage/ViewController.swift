4f5f39342f8b83c92597db872a7bc32382fae4ea
Pooya Hatami <pooya@node-1w7jr9qro06sw04hcipbybhw2.ipv6.telus.net>
2018-09-06 16:13:50 -0700

    done

--- IOS-Swift-CustomImage/ViewController.swift
+++ IOS-Swift-CustomImage/ViewController.swift
@@ -10,15 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewImage1 : UIImageView!
+    @IBOutlet weak var viewImage2 : UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        viewImage1.layer.borderWidth = 5
+        viewImage1.layer.borderColor = UIColor(red: 0.8, green: 0.8, blue: 0.4, alpha: 0.8).cgColor
+        viewImage1.layer.cornerRadius = 20.0
+        viewImage1.layer.masksToBounds = true
+        
+        
+        viewImage2.layer.shadowColor = UIColor.blue.cgColor
+        viewImage2.layer.shadowOffset = CGSize(width: 5, height: 5)
+        viewImage2.layer.shadowOpacity = 1
+        viewImage2.layer.shadowRadius = 20
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
